1a7626a7d7feb9ad3d8d07f7371bef98fa5b5b93
Alexey Puchkov <alexey.pkv@gmail.com>
2017-07-31 15:13:46 +0300

    Add unit tests for UnitTestSkeleton

--- tests/Skeleton/UnitTestSkeletonTest.php
+++ tests/Skeleton/UnitTestSkeletonTest.php
@@ -2,6 +2,7 @@
 namespace Skeleton;
 
 
+use Skeleton\Base\ILoader;
 use Skeleton\Base\IMap;
 use Skeleton\Base\IContextReference;
 
@@ -9,11 +10,16 @@ use Skeleton\Base\IContextReference;
 class UnitTestSkeletonTest extends \SkeletonTestCase
 {
 	/**
+	 * @param IMap|\PHPUnit_Framework_MockObject_MockObject $map
 	 * @return \PHPUnit_Framework_MockObject_MockObject|Skeleton
 	 */
 	private function mockSkeleton(&$map)
 	{
+		$loader = $this->getMock(ILoader::class);
+		
 		$map = $this->getMock(IMap::class);
+		$map->method('loader')->willReturn($loader);
+		
 		$skeleton = $this->getMock(Skeleton::class);
 		$skeleton->method('getMap')->willReturn($map);
 		return $skeleton;
@@ -119,6 +125,109 @@ class UnitTestSkeletonTest extends \SkeletonTestCase
 			});
 
 		$testSkeleton->get('n');
+	}
+	
+	public function test_get_ContextPassedAsArray_ContextCreatedFromArray()
+	{
+		$skeleton = $this->mockSkeleton($map);
+		
+		$testSkeleton = new UnitTestSkeleton($skeleton);
+
+		/** @var $map \PHPUnit_Framework_MockObject_MockObject  */
+		$map->expects($this->once())
+			->method('get')
+			->willReturnCallback(function ($a, IContextReference $context)
+			{
+				$this->assertEquals('b', $context->context()->get('a'));
+			});
+
+		$testSkeleton->get('n', ['a' => 'b']);
+	}
+	
+	
+	public function test_load_LoadOnLoaderCalled()
+	{
+		$testSkeleton = new UnitTestSkeleton($this->mockSkeleton($map));
+		
+		/** @var IMap $map */
+		/** @var \PHPUnit_Framework_MockObject_MockObject $loader */
+		$loader = $map->loader();
+		
+		$loader->expects($this->once())->method('get');
+
+		$testSkeleton->load(\stdClass::class);
+	}
+	
+	public function test_load_TargetItemPassedToLoader()
+	{
+		$testSkeleton = new UnitTestSkeleton($this->mockSkeleton($map));
+		
+		/** @var IMap $map */
+		/** @var \PHPUnit_Framework_MockObject_MockObject $loader */
+		$loader = $map->loader();
+		
+		$loader->method('get')->with(\stdClass::class);
+
+		$testSkeleton->load(\stdClass::class);
+	}
+	
+	public function test_load_ContextNotPassed_NullPassedToLoader()
+	{
+		$testSkeleton = new UnitTestSkeleton($this->mockSkeleton($map));
+		
+		/** @var IMap $map */
+		/** @var \PHPUnit_Framework_MockObject_MockObject $loader */
+		$loader = $map->loader();
+		
+		$loader->method('get')->with($this->anything(), null);
+
+		$testSkeleton->load(\stdClass::class);
+	}
+	
+	public function test_load_LoaderResultReturned()
+	{
+		$testSkeleton = new UnitTestSkeleton($this->mockSkeleton($map));
+		
+		/** @var IMap $map */
+		/** @var \PHPUnit_Framework_MockObject_MockObject $loader */
+		$loader = $map->loader();
+		
+		$loader->method('get')->willReturn(123);
+
+		self::assertEquals(123, $testSkeleton->load(\stdClass::class));
+	}
+	
+	public function test_load_ContextPassedToLoader()
+	{
+		$testSkeleton = new UnitTestSkeleton($this->mockSkeleton($map));
+		$context = $this->getMock(IContextReference::class);
+		
+		/** @var IMap $map */
+		/** @var \PHPUnit_Framework_MockObject_MockObject $loader */
+		$loader = $map->loader();
+		
+		$loader->method('get')->willReturnCallback(function ($a, $b) use ($context)
+		{
+			self::assertEquals($context, $b);
+		});
+
+		$testSkeleton->load(\stdClass::class, $context);
+	}
+	
+	public function test_load_ArrayAsContextPassed_ContextFromArrayCreated()
+	{
+		$testSkeleton = new UnitTestSkeleton($this->mockSkeleton($map));
+		
+		/** @var IMap $map */
+		/** @var \PHPUnit_Framework_MockObject_MockObject $loader */
+		$loader = $map->loader();
+		
+		$loader->method('get')->willReturnCallback(function ($a, IContextReference $b)
+		{
+			self::assertEquals(123, $b->context()->get('a'));
+		});
+
+		$testSkeleton->load(\stdClass::class, ['a' => 123]);
 	}
 	
 	
