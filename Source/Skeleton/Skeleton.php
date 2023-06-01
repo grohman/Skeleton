e32c43877ac8926d2ee0120d1d7bb27435707417
Daniel Podrabinek <dpodrabinek@oktopost.com>
2023-06-01 17:17:35 +0300

    Fixed setting configLimitLen if $namespaceLimit is null

--- Source/Skeleton/Skeleton.php
+++ Source/Skeleton/Skeleton.php
@@ -97,7 +97,7 @@ class Skeleton implements ISkeletonSource, IBoneConstructor
 		{
 			$loader->setBoneConstructor($this);
 			$this->configLimit = $namespaceLimit;
-			$this->configLimitLen = strlen($this->configLimit);
+			$this->configLimitLen = $namespaceLimit ? strlen($namespaceLimit) : 0;
 		}
 		
 		$this->configLoader = $loader;
